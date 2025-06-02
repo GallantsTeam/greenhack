087d2292ca45f0c64db24db63dd4be90874e8956
Cloud Host <digital.galant@gmail.com>
2025-06-02 08:37:44 +0000

    Чё т я не понял. А где модальное окно активации, где весь функционал который мы делали? Почему все пропало? ВЕРНИ.

---
+++ .next/static/chunks/src_app_games_[gameId]_page_tsx_4e4c0b1d._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_games_[gameId]_page_tsx_4e4c0b1d._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_games_[gameId]_page_tsx_4e4c0b1d._.js",
+  "chunks": [
+    "static/chunks/src_components_CategoryScroller_tsx_7ec411b3._.js"
+  ],
+  "source": "dynamic"
+});
